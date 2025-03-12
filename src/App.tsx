804bbbb6a71581cef232ddc4e3324cc1dd102ca1
BrijenMakwana <brijenma@gmail.com>
2025-03-12 17:48:14 +0530

    GameCard UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import "./App.css";
+import GameCard from "./components/GameCard.jsx";
 import SearchBar from "./components/SearchBar.jsx";
 
 export function App() {
   return (
     <view className="container">
       <SearchBar />
+
+      <list
+        className="list-container"
+        list-type="flow"
+        span-count={2}
+        scroll-orientation="vertical"
+      >
+        {Array.from({ length: 40 }).map((item, index) => {
+          return (
+            <list-item
+              item-key={`list-item-${index}`}
+              key={`list-item-${index}`}
+            >
+              <GameCard />
+            </list-item>
+          );
+        })}
+      </list>
     </view>
   );
 }
